2629a912f840bceff85da39f6796a4623c77a638
Audrey Altman <audrey@dp.la>
2014-12-18 10:58:02 -0500

    current progress - not working

--- app/models/krikri/validation_report.rb
+++ app/models/krikri/validation_report.rb
@@ -1,11 +1,6 @@
 module Krikri
   # Provides data for validation reports related to the QA process
-  # Uses IndexService to get info about items with missing required fields
   class ValidationReport
-
-    REQUIRED_FIELDS = ['dataProvider_name', 'isShownAt_id', 'preview_id',
-                       'sourceResource_rights', 'sourceResource_title',
-                       'sourceResource_type_id']
 
     ##
     # Returns the number of items missing each required field
--- lib/krikri.rb
+++ lib/krikri.rb
@@ -5,4 +5,5 @@ require 'blacklight'
 module Krikri
   # autoload libraries
   autoload :IndexService,       'krikri/index_service'
+  autoload :ValidationReport,   'krikri/validation_report'
 end
