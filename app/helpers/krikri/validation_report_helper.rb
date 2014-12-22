dec3425e3afc518d2cee7e9ca0417b37d777c7a0
Audrey Altman <audrey@dp.la>
2014-12-22 12:59:43 -0500

    create validation report list model

--- app/helpers/krikri/validation_report_helper.rb
+++ app/helpers/krikri/validation_report_helper.rb
@@ -1,56 +1,20 @@
 module Krikri
   module ValidationReportHelper
 
-    REQUIRED_FIELDS = ['dataProvider_name', 'isShownAt_id', 'preview_id',
-                 'sourceResource_rights', 'sourceResource_title',
-                 'sourceResource_type_id']
-
     def report_list
-      construct_report(missing_field_totals)
+      Krikri::ValidationReportList.new(blacklight_config).report_list
     end
 
-    ##
-    # Returns the number of items missing each required field
-    # @return Array
-    def missing_field_totals
-      solr_params = { 
-        :rows => 0,
-        'facet.field' => REQUIRED_FIELDS,
-        'facet.mincount' => 10000000,
-        'facet.missing' => true
-      }
-      Blacklight::SolrRepository.new(blacklight_config).search(solr_params)  
-    end
+    # # Generate a link for a single report from report_list
+    # # @param Hash
+    # def display_link(report)
 
-    # transform Hash of key-value pairs into Array of Hashes
-    #   example: { "field_name"=>[nil,2] } is tranformed into
-    #   [{ name: "field_name", count: 2 }]
-    def construct_report(solr_response)
+    #   if report[:link_url]
+    #     return link_to report[:label] report[:link_url]
+    #   end
 
-      if solr_response && solr_response['facet_counts'] && 
-        fields = solr_response['facet_counts']['facet_fields']
-
-        return fields.each_with_object([]) do |(key, value), array|
-          array << { 
-            name: key, 
-            count: value[1],
-          }
-        end
-      end
-
-      return nil
-    end
-
-    def report_link(report)
-      label = "#{report[:name]} (#{report[:count]})"
-
-      if report[:count] > 0
-        link_url = "validation_reports?q=-#{report[:name]}:[*%20TO%20*]"
-        return link_to label, link_url
-      end
-
-     return label
-    end
+    #   return report[:label]
+    # end
 
   end
 end
---
+++ app/models/krikri/validation_report_list.rb
@@ -0,0 +1,57 @@
+module Krikri
+  class ValidationReportList
+    attr_reader :report_list
+
+    REQUIRED_FIELDS = ['dataProvider_name', 'isShownAt_id', 'preview_id',
+                 'sourceResource_rights', 'sourceResource_title',
+                 'sourceResource_type_id']
+
+    def initialize(blacklight_config)
+      @blacklight_config = blacklight_config
+      @report_list = list_for_display(missing_field_totals)
+    end
+
+    ##
+    # Returns the number of items missing each required field
+    # @return Array
+    def missing_field_totals
+      solr_params = { 
+        :rows => 0,
+        'facet.field' => REQUIRED_FIELDS,
+        'facet.mincount' => 10000000,
+        'facet.missing' => true
+      }
+      Blacklight::SolrRepository.new(@blacklight_config).search(solr_params)  
+    end
+
+    # Transform Hash of key-value pairs into Array of Hashes.
+    #   example: { "field_name"=>[nil,2] } is tranformed into
+    #   [{ :label => "field_name (2)", :link_url => "validation_reports?[...]"}]
+    def list_for_display(solr_response)
+
+      if solr_response && solr_response['facet_counts'] && 
+        fields = solr_response['facet_counts']['facet_fields']
+
+        return fields.each_with_object([]) do |(key, value), array|
+          array << report_link(key, value[1])
+        end
+      end
+
+      return nil
+    end
+
+    # Construct a link and display label for a validation report
+    # @param: String, int
+    # @return: Hash
+    def report_link(name, count)
+      link = { :label => "#{name} (#{count})" }
+
+      if count > 0
+        link[:url] = "validation_reports?q=-#{name}:[*%20TO%20*]"
+      end
+
+     return link
+    end
+
+  end
+end
