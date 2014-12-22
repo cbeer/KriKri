fb8af0105b16d1ec2fd5f69f99308ac5b566f27e
Audrey Altman <audrey@dp.la>
2014-12-22 18:03:48 -0500

    solr validation report spec

---
+++ spec/models/solr_validation_report_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Krikri::SolrValidationReport do 
+
+  subject { Krikri::SolrValidationReport.new({}) }
+
+  describe '#to_partial_path' do
+
+    it 'should return validation_reports/document path' do
+      expect(subject.to_partial_path).to eq('validation_reports/document')
+    end
+
+  end
+
+end
