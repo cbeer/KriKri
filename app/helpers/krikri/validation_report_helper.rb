fcefabc62386612db2ef5baa9de6e4f9ffaa51b4
Audrey Altman <audrey@dp.la>
2014-12-22 13:02:35 -0500

    remove unused method

--- app/helpers/krikri/validation_report_helper.rb
+++ app/helpers/krikri/validation_report_helper.rb
@@ -5,16 +5,5 @@ module Krikri
       Krikri::ValidationReportList.new(blacklight_config).report_list
     end
 
-    # # Generate a link for a single report from report_list
-    # # @param Hash
-    # def display_link(report)
-
-    #   if report[:link_url]
-    #     return link_to report[:label] report[:link_url]
-    #   end
-
-    #   return report[:label]
-    # end
-
   end
 end
